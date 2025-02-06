baeb224320494036bd8ed52a68859bcf17875de1
19840769 <kodikarah.21@uom.lk>
2025-02-06 12:35:50 +0530

    (change)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,20 @@
 @section('content')
 <h1>List of Tasks</h1>
 <div>
+    @if(session()->has("success"))
+    <div class="alert-success">
+        {{session()->get("success")}}
+    </div>
+    @endif
+    @if (session("error"))
+    <div class="alert alert-danger">
+        {{session("error")}}
+    </div>
+
+    @endif
+
     @foreach ($tasks as $task)
-    <p>{{$task-> title}}</p>
+    {{$task}}
     @endforeach
 
 </div>
