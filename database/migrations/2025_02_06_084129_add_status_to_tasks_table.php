99ecfc656ce387e5e0468c3a7549ea7afc0957f7
19840769 <kodikarah.21@uom.lk>
2025-02-06 14:15:25 +0530

    (change)migration

--- database/migrations/2025_02_06_084129_add_status_to_tasks_table.php
+++ database/migrations/2025_02_06_084129_add_status_to_tasks_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
 {
     /**
@@ -12,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->string('deadline');
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status')->nulable();
         });
     }
 
@@ -26,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status');
+        });
     }
 };
